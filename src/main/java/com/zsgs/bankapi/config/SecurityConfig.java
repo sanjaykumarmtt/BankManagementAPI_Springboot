dee687a074ec6607accbee9eb64989a3432b7afe
Sanjay K <sanjays82943@gmail.com>
2026-06-01 13:30:13 +0530

    Updated Cookie configuration and littal Error I ratifaiy

--- src/main/java/com/zsgs/bankapi/config/SecurityConfig.java
+++ src/main/java/com/zsgs/bankapi/config/SecurityConfig.java
@@ -23,76 +23,79 @@ import com.zsgs.bankapi.service.managerauthservice.AuthService;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//	@Autowired
-//	;
-	
+    // @Autowired
+    // ;
+
     @Bean
     @Lazy
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-//    
+
+    //
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,JWtFilter jWtFilter) throws Exception {
-    	
-    	
-    	http
-        .cors(cors -> cors.configurationSource(request -> {
-            var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-       
-       corsConfiguration.setAllowedOriginPatterns(List.of("*"));
-            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-            corsConfiguration.setAllowedHeaders(List.of("*"));
-            corsConfiguration.setAllowCredentials(true);
-            return corsConfiguration;}))
- 
-            .authorizeHttpRequests(auth -> auth
-            		.requestMatchers("/api/auth/**").permitAll()
-            		.requestMatchers("/api/accounts/review/?*").permitAll()
-            		.requestMatchers("/api/accounts/review/manager/?*").hasAnyAuthority("MANAGER","USER")
-            		.requestMatchers("/api/accounts/manager/search/?*").hasAuthority("MANAGER")
-            		.requestMatchers("/api/accounts/user/**").hasAuthority("USER")
-            		.requestMatchers("/api/accounts/**").hasAuthority("MANAGER")
-            		
-            		.anyRequest().authenticated());
-//        http.formLogin(form->form.permitAll().defaultSuccessUrl("/api/auth/defalt"));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JWtFilter jWtFilter) throws Exception {
+
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+
+                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    return corsConfiguration;
+                }))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/accounts/review/?*").permitAll()
+                        .requestMatchers("/api/accounts/review/manager/?*").hasAnyAuthority("MANAGER", "USER")
+                        .requestMatchers("/api/accounts/manager/search/?*").hasAuthority("MANAGER")
+                        .requestMatchers("/api/accounts/user/**").hasAuthority("USER")
+                        .requestMatchers("/api/accounts/**").hasAuthority("MANAGER")
+
+                        .anyRequest().authenticated());
+        // http.formLogin(form->form.permitAll().defaultSuccessUrl("/api/auth/defalt"));
         http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(jWtFilter, UsernamePasswordAuthenticationFilter.class);
-        
+
         return http.build();
     }
+
     private Object requestMatchers(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Bean
-    public UserDetailsService userDetailService() {
-    	
-//    	UserDetails user=User.withUsername("sanjay")
-//    			.password(paseeEncoder.encode("sanjay123"))
-//    			.roles("USER")
-//    			.build();
-//    	System.out.print(user);
-//    	UserDetails admin=User.withUsername("kumar")
-//    			.password(paseeEncoder.encode("sanjay123"))
-//    			.roles("ADMIN")
-//    			.build();
-    	return new AuthService();
+        // TODO Auto-generated method stub
+        return null;
     }
-    
+
+    @Bean
+    public UserDetailsService userDetailService() {
+
+        // UserDetails user=User.withUsername("sanjay")
+        // .password(paseeEncoder.encode("sanjay123"))
+        // .roles("USER")
+        // .build();
+        // System.out.print(user);
+        // UserDetails admin=User.withUsername("kumar")
+        // .password(paseeEncoder.encode("sanjay123"))
+        // .roles("ADMIN")
+        // .build();
+        return new AuthService();
+    }
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-    	DaoAuthenticationProvider authDAO=new DaoAuthenticationProvider();
-    	authDAO.setUserDetailsService(userDetailService());
-    	authDAO.setPasswordEncoder(passwordEncoder());
-    	
-    	return authDAO;
+        DaoAuthenticationProvider authDAO = new DaoAuthenticationProvider();
+        authDAO.setUserDetailsService(userDetailService());
+        authDAO.setPasswordEncoder(passwordEncoder());
+
+        return authDAO;
     }
-    
+
     @Bean
     public AuthenticationManager authenticationManager() {
-    	return new ProviderManager(List.of(authenticationProvider()));
+        return new ProviderManager(List.of(authenticationProvider()));
     }
-    
+
 }
